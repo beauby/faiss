c288d63920a85bc5768a2fe3c74f4a31c55126da
Lucas Hosseini <hoss@fb.com>
2019-01-23 11:25:58 +0100

    Update build.

--- conda/faiss-gpu/build.sh
+++ conda/faiss-gpu/build.sh
@@ -1,8 +1,9 @@
-# NOTE(beauby): This is needed as conda does not symlink `python-config`.
-./configure --with-python-config=python${PY_VER}-config
+./configure
+cat makefile.inc
 make -j $CPU_COUNT
 make -C gpu -j $CPU_COUNT
 cd python
+make cpu
 make gpu
 make build
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
