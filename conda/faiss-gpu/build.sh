312ef26666e941da3e9117a1cd4e390024d58a42
Lucas Hosseini <hoss@fb.com>
2019-01-29 20:20:40 +0100

    Fix faiss-gpu/build.sh

--- conda/faiss-gpu/build.sh
+++ conda/faiss-gpu/build.sh
@@ -1,9 +1,5 @@
 ./configure
-cat makefile.inc
 make -j $CPU_COUNT
-make -C gpu -j $CPU_COUNT
+make -C python
 cd python
-make cpu
-make gpu
-make build
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
