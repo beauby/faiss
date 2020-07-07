cb0b7d0ef17802c8d03e16329abe20e24d7c5999
Lucas Hosseini <lucsa.hosseini@gmail.com>
2020-07-07 20:09:20 +0200

    Fix unix header + format specifier.

--- faiss/IndexBinaryHNSW.cpp
+++ faiss/IndexBinaryHNSW.cpp
@@ -23,8 +23,11 @@
 
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
 #include <stdint.h>
+
+#ifndef _MSC_VER
+#include <unistd.h>
+#endif // !_MSC_VER
 
 #include <faiss/utils/random.h>
 #include <faiss/utils/Heap.h>
@@ -52,7 +55,7 @@ void hnsw_add_vertices(IndexBinaryHNSW& index_hnsw,
   size_t ntotal = n0 + n;
   double t0 = getmillisecs();
   if (verbose) {
-    printf("hnsw_add_vertices: adding %ld elements on top of %ld "
+    printf("hnsw_add_vertices: adding %zd elements on top of %zd "
            "(preset_levels=%d)\n",
            n, n0, int(preset_levels));
   }
